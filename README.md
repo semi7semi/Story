112ac7c955ee9fb19317a63c93b3d72c4e89037a
Marcin <sommer.marcin@gmail.com>
2021-09-14 12:08:03 +0200

    css v2

---
+++ README.md
@@ -0,0 +1,9 @@
+# Magda-App - Heroku deploy
+
+## Aplikacja dla Magdy
+
+
+## Opis
+
+Aplikacja z opowiadaniami erotycznymi dla ukochanej :)
+
--- story_app/forms.py
+++ story_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Story
 
 class AddStory(forms.ModelForm):
-    code = forms.CharField(max_length=8, label="Kod")
+    code = forms.CharField(max_length=8, label="Kod", widget=forms.TextInput(attrs={'class': "form-control"}))
     class Meta:
         model = Story
         fields = "__all__"
@@ -10,4 +10,9 @@ class AddStory(forms.ModelForm):
             "title": "Tytuł",
             "type": "Rodzaj",
             "plot": "Treść",
+        }
+        widgets = {
+            "title": forms.TextInput(attrs={'class': "form-control"}),
+            "type": forms.Select(attrs={'class': "form-control"}),
+            "plot": forms.Textarea(attrs={'class': "form-control", 'cols': 100, 'rows': 100}),
         }
