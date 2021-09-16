7d9bab59d87f4c049fe5bd44147301d6f4f09cc8
Marcin <sommer.marcin@gmail.com>
2021-09-17 01:58:36 +0200

    fix2

--- Story/settings.py
+++ Story/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*)224jc0dn_rd06v!1q@p$=zxd#utp0r815vlw_p*05^c#ra@*'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-*)224jc0dn_rd06v!1q@p$=zxd#utp0r815vlw_p*05^c#ra@*'
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
