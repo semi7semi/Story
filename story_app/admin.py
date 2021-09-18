81478f3968a69b46c144fcb9e33aaf14aab8241d
Marcin <sommer.marcin@gmail.com>
2021-09-18 14:16:36 +0200

    admin

--- story_app/admin.py
+++ story_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from story_app.models import Story
+
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "type", "plot", "publication_date")
+    list_editable = ("type", "plot", "publication_date")
+
+admin.site.register(Story, StoryAdmin)
