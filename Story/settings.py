33c7ac7b717ccc69374fe2a5a9109b8123bebcff
Marcin <sommer.marcin@gmail.com>
2021-09-18 11:17:29 +0200

    deploy10

--- Story/settings.py
+++ Story/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://dla-magdy.herokuapp.com/',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
